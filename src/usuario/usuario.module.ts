f219c2f41f886db81b713d6466b68f53460ec06c
joaohenrique9 <joao.gh61@gmail.com>
2023-09-11 09:07:55 -0300

    Injeção de dependências

--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { UsuarioController } from "./usuario.controller";
+import { UsuarioRepository } from "./usuario.repository";
 
 @Module({
-    imports: [],
     controllers: [UsuarioController],
-    providers: [],
+    providers: [UsuarioRepository],
   })
   export class UsuarioModule {}
