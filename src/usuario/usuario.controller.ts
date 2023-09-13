d975d73478ce99884dbc29a209aec28939a83f3d
joaohenrique9 <joao.gh61@gmail.com>
2023-09-13 00:21:50 -0300

    Validando o usuário

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
+import { CriaUsuarioDTO } from "./dto/criaUsuario.dto";
 
 @Controller('/usuarios')
 export class UsuarioController{
@@ -7,7 +8,7 @@ export class UsuarioController{
     constructor(private usuarioRepository: UsuarioRepository) {}
 
     @Post()
-    criarUsuario(@Body() dadosDoUsuario){
+    criarUsuario(@Body() dadosDoUsuario: CriaUsuarioDTO){
         this.usuarioRepository.salvar(dadosDoUsuario);
         return dadosDoUsuario;
     }
